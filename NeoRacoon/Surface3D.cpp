aeb8474b49121f44cf82a4e3016106409e740762
charline_rochelle <charline.rochelle@gmail.com>
2017-05-10 18:33:34 +0200

    size

--- NeoRacoon/Surface3D.cpp
+++ NeoRacoon/Surface3D.cpp
@@ -10,6 +10,11 @@ bool Surface3D::get_Close ( ) const
 	return this->_Close;
 }
 
+int Surface3D::get_NbrEdges ( ) const
+{
+	return ( this->_Close ) ? this->_Edges.size ( ) : this->_Edges.size ( ) + 1;
+}
+
 Surface3D::~Surface3D ( )
 {
 }
